85ac1afb2a1f2476747b00057057679341495157
Shawn-Checkly <shawn-checkly@Shawn-Checklys-MacBook-Air.local>
2026-04-16 23:59:24 -0400

    clean package lock file

---
+++ src/__checks__/synthetics/playwrightCheck.check.ts
@@ -0,0 +1,11 @@
+import { PlaywrightCheck } from 'checkly/constructs'
+import path from 'path'
+
+export const playwrightCheckSuite = new PlaywrightCheck('checkflix-playwright-suite', {
+  name: 'Checkflix Playwright Suite',
+  description: 'Runs the Checkflix UI and API tests across multiple browsers using Playwright Test.',
+  playwrightConfigPath: path.join(__dirname, '../../playwright.config.ts'),
+  pwProjects: ['chromium', 'firefox', 'webkit'],
+  pwTags: ['@pwcs'],
+  include: ['tests/**/*'],
+})
